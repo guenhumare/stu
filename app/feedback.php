6e9183b82602c0382c50e6f7e05a53ce44fea63a
Alexander.Shvedov <shvedov89@gmail.com>
2014-04-20 19:46:30 +0400

    bug

--- app/feedback.php
+++ app/feedback.php
@@ -5,7 +5,7 @@ $name = check_input($_POST['name'], "Вы не ввели свое имя");
 $email = check_input($_POST['email']);
 $telephone = check_input($_POST['telephone']);
 $message = check_input($_POST['message'], "Вы не указали текст сообщения");
-#way = $_POST['way'];
+$contact_way = $_POST['way'];
 
 if ($email == '') {
   if ($telephone == '') {
@@ -22,7 +22,7 @@ $message = "
 Имя: $name
 E-mail: $email
 Телефон: $telephone
-Способ связи: $way
+Способ связи: $contact_way
 
 Message:
 $message";
@@ -35,13 +35,10 @@ $headerFields = array(
 
 mail($targetMail, $subject, $message, implode("\r\n", $headerFields));
 
-//header('Location: thanks.html');
 echo "Ваше сообщение успешно отправлено";
 exit();
 
-/* Functions we used */
-function check_input($data, $problem='')
-{
+function check_input($data, $problem='') {
   $data = trim($data);
   $data = stripslashes($data);
   $data = htmlspecialchars($data);
@@ -52,8 +49,7 @@ function check_input($data, $problem='')
   return $data;
 }
 
-function show_error($myError)
-{
+function show_error($myError) {
   header('HTTP/1.0 400 Bad Request', true, 400);
   echo $myError;
   exit();
